3db305c3bbc3fe42392c9c9435eaedd60caf52bf
Kush Patel <55899517+kp673@users.noreply.github.com>
2022-03-09 16:42:54 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Parse from 'parse';
+import Table from 'react-bootstrap/Table'
 
 function App() {
+  async function getData() {
+    const Ratings = Parse.Object.extend('Ratings');
+  const query = new Parse.Query(Ratings);
+  try {
+   const results = await query.find();
+    for (const object of results) {
+      // Access the Parse Object attributes using the .GET method
+      const beer = object.get('beer')
+      const hard = object.get('hard')
+      const eat = object.get('eat')
+      const smoking = object.get('smoking')
+      const name = object.get('name')
+      const avgRating = object.get('avgRating')
+      const iq = object.get('iq')
+      const wRating = object.get('wRating')
+      console.log(name);
+      console.log(beer);
+      console.log(hard);
+      console.log(smoking);
+      console.log(eat);
+      console.log(iq);
+      console.log(avgRating);
+      console.log(wRating);
+    }
+  } catch (error) {
+    console.error('Error while fetching Ratings', error);
+  }
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan={2}>Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Parse from 'parse';
+
+
+Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+// Remember to inform BOTH the Back4App Application ID AND the JavaScript KEY
+Parse.initialize(
+  'sRjlTukJ2ph5XAgrF5OmbIoibhk17N4JAOJb1wgb', // This is your Application ID
+  '0Esq6dKZqpiBT80LlRKh5deHH9YJcv4iEuBjmzFx');
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +17,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
